a955958513a66a09ee0466d1bf6464698b5febd8
architsangal <architsangal2000@gmail.com>
2023-03-11 00:31:02 +0530

    made a maven project and added dependencies to it

---
+++ src/calculator/src/main/java/org/example/Calculator.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Calculator
+{
+    Calculator()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
