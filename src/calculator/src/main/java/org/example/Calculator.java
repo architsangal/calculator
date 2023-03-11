100236751582e2d06ce34b4fac9b8a9c0019fc38
architsangal <architsangal2000@gmail.com>
2023-03-11 09:35:14 +0530

    made a new branch

--- src/calculator/src/main/java/org/example/Calculator.java
+++ src/calculator/src/main/java/org/example/Calculator.java
@@ -2,18 +2,20 @@ package org.example;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator
 {
+    private Logger logger;
+
     Calculator()
     {
-
+        logger  = LogManager.getLogger(Calculator.class);
     }
 
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        Calculator calc = new Calculator();
+        Scanner sc = new Scanner(System.in);
     }
 }
