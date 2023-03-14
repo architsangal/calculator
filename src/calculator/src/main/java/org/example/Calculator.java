feeb2729f682ccfb3eccad5bb4e0231db8a2c97e
architsangal <architsangal2000@gmail.com>
2023-03-14 18:30:43 +0530

    fixed a bug

--- src/calculator/src/main/java/org/example/Calculator.java
+++ src/calculator/src/main/java/org/example/Calculator.java
@@ -185,7 +185,7 @@ public class Calculator
             int ch = 0;
 
             String choice = inputDouble(sc);
-            if(choice.equals("no") && (int)Double.parseDouble(choice) != Double.parseDouble(choice))
+            if(!choice.equals("no") && (int)Double.parseDouble(choice) != Double.parseDouble(choice))
             {
                 continue;
             }
